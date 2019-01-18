5ff4c1710fcc212a71684fafad8e8641059ccdb6
costagavras <cgavras@yahoo.ca>
2019-01-17 23:56:59 -0500

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root "welcome#index"
+
+  resources :recipes do
+    resources :comments
+  end
+
+  resources :categories
+  resources :users
+  resources :sessions
+
 end
