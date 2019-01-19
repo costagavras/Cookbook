de802b0c7dfc96c0e8e935a009b6ab60dce06672
costagavras <cgavras@yahoo.ca>
2019-01-18 21:28:09 -0500

    associations, seeds, routes

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_secure_password
 
   has_many :recipes, dependent: :destroy #deletes dependencies when deleting a recipe
-  has_many :comments, through: :recipes #when user has a recipe that has comments from different users
+  # has_many :comments, through: :recipes #when user has a recipe that has comments from different users
   has_many :comments #comments left by the user himself
 
   validates :name, presence: true
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  get 'welcome/index'
   root "welcome#index"
+
+  get '/welcome/about_me', :as => 'about'
 
   resources :recipes do
     resources :comments
