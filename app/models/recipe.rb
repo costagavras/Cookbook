2a48c72f3883ec98458aa0f35b9542a164d8e312
costagavras <cgavras@yahoo.ca>
2019-01-20 22:58:42 -0500

    css front page

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -6,4 +6,20 @@ class Recipe < ApplicationRecord
 
   # has_many_attached :images
 
+  validates :name, :complexity, :category, :prep_time,:servings, :grabbed, presence: true
+  # validates :hunt_date, inclusion:{ in: (Date.today-6.months..Date.today+6.months)}
+  # validates :hunt_time, inclusion:{ in: (8.hours..20.hours)}
+  validates :servings, inclusion: { in: (1..20)}
+
+  def difflevel_int_to_text
+    case self.complexity
+    when 1
+      return "Easy"
+    when 2
+      return "Average"
+    when 3
+      return "Hard"
+    end
+  end
+
 end
