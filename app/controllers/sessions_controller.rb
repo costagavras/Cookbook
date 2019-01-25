a43f09f973c8f68352485e3792f09fbf065ad539
costagavras <cgavras@yahoo.ca>
2019-01-24 19:43:08 -0500

    comments logic and css

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -24,7 +24,7 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:user_id] = nil
-    redirect_to request.referer, info: "Logged Out!"
+    redirect_to root_url, info: "Logged Out!"
   end
 
 end
