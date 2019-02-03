34280c175f1c680dae1dea911de43c192929a495
costagavras <cgavras@yahoo.ca>
2019-02-02 19:33:19 -0500

    horizontal scroll bar in recipe show

--- app/assets/javascripts/recipe.js
+++ app/assets/javascripts/recipe.js
@@ -8,3 +8,12 @@ function show_loader(){
   var loader = document.querySelector(".loader");
   loader.style.visibility = "visible";
 }
+
+// When the user scrolls the photo window, execute myFunction
+function horizontal_bar() {
+  var photoScroll = document.querySelector(".photo");
+  var photoScrollTop = photoScroll.scrollTop;
+  var height = photoScroll.scrollHeight - photoScroll.clientHeight; //scrollHeight less scrolled distance, always decreasing
+  var scrolled = (photoScrollTop / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
