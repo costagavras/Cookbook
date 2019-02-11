7a4040cd9b0dc5d1693acc4645ee3b9aaad1c801
costagavras <cgavras@yahoo.ca>
2019-02-10 19:46:07 -0500

    filter scopes

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -21,6 +21,19 @@ class Recipe < ApplicationRecord
   validates :servings, inclusion: { in: (1..20)}
   validates :screencapture_name, format: {without: /\s/ }
 
+  scope :prep_time_longer_than, ->(prep_time) {where("prep_time > ?", prep_time)}
+  scope :prep_time_shorter_than, ->(prep_time) {where("prep_time < ?", prep_time)}
+  scope :prep_time_equal_to, ->(prep_time) {where("prep_time = ?", prep_time)}
+
+  scope :servings_more_than, ->(servings) {where("servings > ?", servings)}
+  scope :servings_less_than, ->(servings) {where("servings < ?", servings)}
+  scope :servings_equal_to, ->(servings) {where("servings = ?", servings)}
+
+  scope :difficulty_more_than, ->(difficulty) {where("difficulty > ?", difficulty)}
+  scope :difficulty_less_than, ->(difficulty) {where("difficulty < ?", difficulty)}
+  scope :difficulty_equal_to, ->(difficulty) {where("difficulty = ?", difficulty)}
+
+
   def self.difflevel_int_to_text
     case self.complexity
     when 1
