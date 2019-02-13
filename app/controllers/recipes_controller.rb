26a1ad27289755b7735e6cf41b00b696eab6bec2
costagavras <cgavras@yahoo.ca>
2019-02-12 23:59:40 -0500

    I18n

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -10,47 +10,44 @@ class RecipesController < ApplicationController
 
   def index
     if filtering_params_exist?
-      if params[:condition] != nil
-        if params[:condition] == "or"
-          @filtered_recipes = []
-          filtering_params(params).each do |key, value|
-            #needs to be transofrmed to an array if not it's an ActiveRecord::Relation instance)
-            @filtered_recipes << @user_recipes.public_send(key, value).to_a if value.present?
-          end
-        elsif params[:condition] == "and"
-          @user_recipes_temp = @user_recipes
-          filtering_params(params).each do |key, value|
-            @filtered_recipes = @user_recipes.public_send(key, value) if value.present?
-            @user_recipes = @filtered_recipes if @filtered_recipes != nil
-            #check necessary to avoir @filtered_recipes becoming nil if first (key,value) return nil result
-          end
-          @filtered_recipes = @user_recipes.to_a
-          @user_recipes = @user_recipes_temp
+      if params[:condition] == "or"
+        @filtered_recipes = []
+        filtering_params(params).each do |key, value|
+          #needs to be transofrmed to an array if not it's an ActiveRecord::Relation instance)
+          @filtered_recipes << @user_recipes.public_send(key, value).to_a if value.present?
         end
-        #needs to be flattened if not it remains a double array (after .to_a)
-        @filtered_recipes.flatten!
-        @filtered_recipes = @filtered_recipes.uniq
-        puts @filtered_recipes.count
-        puts @filtered_recipes.inspect
-        if @filtered_recipes.empty?
-          @filtered_result = false
-          @filter_result_message = "This filter returns 0 recipes"
-        elsif !@filtered_recipes.empty? && @filtered_recipes.count == @user_recipes.count
-          @filtered_result = false
-          @filter_result_message = "This filter returns all recipes"
-        else
-          @filtered_result = true
-          @filter_result_message = I18n.t("recipe.search_found") + " #{@filtered_recipes.count}"
-          @filtered_categories = []
-          @filtered_recipes.each do |recipe|
-            if @categories.include?(recipe.category)
-              @filtered_categories << recipe.category
-            end
-          end
-          @filtered_categories = @filtered_categories.uniq
+      elsif params[:condition] == "and"
+        @user_recipes_temp = @user_recipes
+        filtering_params(params).each do |key, value|
+          @filtered_recipes = @user_recipes.public_send(key, value) if value.present?
+          @user_recipes = @filtered_recipes if @filtered_recipes != nil
+          #check necessary to avoir @filtered_recipes becoming nil if first (key,value) return nil result
         end
+        @filtered_recipes = @user_recipes.to_a
+        @user_recipes = @user_recipes_temp
+      end
+      #needs to be flattened if not it remains a double array (after .to_a)
+      @filtered_recipes.flatten!
+      #for some reason it doubles some values in the array
+      @filtered_recipes = @filtered_recipes.uniq
+      # puts @filtered_recipes.count
+      # puts @filtered_recipes.inspect
+      if @filtered_recipes.empty?
+        @filtered_result = false
+        @filter_result_message = "This filter returns 0 recipes"
+      elsif !@filtered_recipes.empty? && @filtered_recipes.count == @user_recipes.count
+        @filtered_result = false
+        @filter_result_message = "This filter returns all recipes"
       else
-        @filter_result_message = "Please set filter condition!"
+        @filtered_result = true
+        @filter_result_message = I18n.t("recipe.search_found") + " #{@filtered_recipes.count}"
+        @filtered_categories = []
+        @filtered_recipes.each do |recipe|
+          if @categories.include?(recipe.category)
+            @filtered_categories << recipe.category
+          end
+        end
+        @filtered_categories = @filtered_categories.uniq
       end
     end
   end
