87f9034b7d41bc94120ddac5aceec1f6b95ac6cb
costagavras <cgavras@yahoo.ca>
2019-02-14 16:30:13 -0500

    routes refacturing

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+#4 custom routes
   get 'welcome/index'
   root "welcome#index"
-
   get '/welcome/about', :as => 'about'
   post '/recipes/search', :as => 'recipe_search'
 
   resources :recipes do
-    resources :comments
+    resources :comments, only: [:new, :create]
   end
 
-  resources :categories
+  resources :categories, only: [:show]
   resources :users
-  resources :sessions
+  resources :sessions,  only: [:new, :create, :destroy]
 
 end
