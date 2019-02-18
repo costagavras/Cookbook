fe3ea03e878cee9dd0cc5fafbe235a5e16475c9a
costagavras <cgavras@yahoo.ca>
2019-02-18 13:22:29 -0500

    static pages and links tests

---
+++ test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,49 @@
+require 'test_helper'
+
+class UsersControllerTest < ActionDispatch::IntegrationTest
+
+  test "should show homepage via welcome" do
+    get welcome_index_path
+    assert_response :success
+    assert_select "title", "Cookbook"
+  end
+
+  test "should show homepage via root" do
+    get root_path
+    assert_response :success
+    assert_select "title", "Cookbook"
+  end
+
+  test "should show about" do
+    get about_path
+    assert_response :success
+    assert_select "title", "Cookbook"
+  end
+
+  # test "should show search result" do
+  #   get recipe_search
+  #   assert_response :success
+  #   assert_select "title", "Cookbook"
+  # end
+  #
+  # test "should show filter result" do
+  #   get recipe_filter
+  #   assert_response :success
+  #   assert_select "title", "Cookbook"
+  # end
+
+  #failing one test on "a[href="/"]"
+  test "layout links" do
+    get root_path
+    assert_template 'welcome/index'
+    assert_select "a[href=?]", root_path
+    assert_select "a[href=?]", user_path
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", "/?locale=en"
+    assert_select "a[href=?]", recipes_path
+    assert_select "a[href=?]", new_recipe_path
+    assert_select "a[href=?]", recipe_search
+    assert_select "a[href=?]", '/', count: 0
+  end
+
+end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -8,29 +8,29 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     @user = users(:one)
   end
 
-  test "user show" do
+  test "should show user" do
     get user_path(@user)
     assert_response :success
   end
 
-  test "create user" do
+  test "should create user" do
     assert_difference("User.count") do
       post users_path, params: {user: {name: "Vanico",
       password: "hello", password_confirmation: "hello"}}
     end
   end
 
-  test "edit user" do
+  test "should edit user" do
     get edit_user_path(@user)
     assert_response :success
   end
 
-  test "update user" do
+  test "should update user" do
     patch user_url(@user), params: {user: {name: "Giorgio",
     password: "123", password_confirmation: "123"}}
   end
 
-  test "delete user" do
+  test "should delete user" do
     assert_difference("User.count", -1) do
       delete user_url(@user)
     end
