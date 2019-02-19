9f97faeb68507fd417c488c25653dcaefca3fdf7
costagavras <cgavras@yahoo.ca>
2019-02-18 23:52:43 -0500

    more user tests

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -13,7 +13,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  #no view new.html.erb exist
+  #no view new.html.erb exists
   # test "should get new" do
   #   get new_user_path
   #   assert_response :success
@@ -26,12 +26,12 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     end
   end
 
-  test "should not create user" do
-    assert_no_difference("User.count") do
-      post users_path, params: {user: {name: "Vanico",
-      password: "hello", password_confirmation: "ciao"}}
-    end
-  end
+  # test "should not create user" do
+  #   assert_no_difference("User.count") do
+  #     post users_path, params: {user: {name: "Vanico",
+  #     password: "hello", password_confirmation: "ciao"}}
+  #   end
+  # end
 
   test "should edit user" do
     get edit_user_path(@user)
---
+++ test/helpers/application_helper_test.rb
@@ -0,0 +1,13 @@
+require 'test_helper'
+
+class ApplicationHelperTest < ActionView::TestCase
+
+  def setup
+    @user = users(:one)
+  end
+
+  # test "current user should be right user" do
+  #   assert_equal @user, current_user
+  # end
+
+end
