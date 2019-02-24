b465dd431bd3d00017f9fc7839cd8e1f3fa434ab
costagavras <cgavras@yahoo.ca>
2019-02-23 23:53:02 -0500

    I18n routes fix

--- config/routes.rb
+++ config/routes.rb
@@ -2,18 +2,25 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 # custom routes
-  get 'welcome/index'
-  root "welcome#index"
-  get '/welcome/about', :as => 'about'
-  post '/recipes/search', to: "recipes#search", :as => 'recipe_search'
-  post '/recipes/filter', to: "recipes#filter", :as => 'recipe_filter'
+  scope ':locale', locale: /en|it|fr|ru/ do
+    root "welcome#index"
+    get 'welcome/index'
+    get '/', to: 'welcome#index'
+    get '/welcome/about', :as => 'about'
+    post '/recipes/search', to: "recipes#search", :as => 'recipe_search'
+    post '/recipes/filter', to: "recipes#filter", :as => 'recipe_filter'
 
-  resources :recipes do
-    resources :comments, only: [:new, :create]
+
+    resources :recipes do
+      resources :comments, only: [:new, :create]
+    end
+
+    resources :categories, only: [:show]
+    resources :users
+    resources :sessions,  only: [:new, :create, :destroy]
+
+
   end
-
-  resources :categories, only: [:show]
-  resources :users
-  resources :sessions,  only: [:new, :create, :destroy]
+  root "welcome#index"
 
 end
