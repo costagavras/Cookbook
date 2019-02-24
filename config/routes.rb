1dcfce96141b7846284b38ae819f9143d095c437
costagavras <cgavras@yahoo.ca>
2019-02-23 23:44:04 -0500

    css bug surf history

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-#4 custom routes
+# custom routes
   get 'welcome/index'
   root "welcome#index"
   get '/welcome/about', :as => 'about'
-  # get '/recipe/search', to: "recipes#search", :as => 'recipes_search'
   post '/recipes/search', to: "recipes#search", :as => 'recipe_search'
   post '/recipes/filter', to: "recipes#filter", :as => 'recipe_filter'
 
