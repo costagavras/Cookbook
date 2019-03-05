500f1df3ce20824b0bc3379e1c6a836a0d737686
costagavras <cgavras@yahoo.ca>
2019-03-04 19:02:02 -0500

    test env. adjustment

--- config/puma.rb
+++ config/puma.rb
@@ -7,6 +7,8 @@
 threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 threads threads_count, threads_count
 
+preload_app!
+
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
 # port        ENV.fetch("PORT") { 3000 }
@@ -15,7 +17,8 @@ port        ENV['PORT'] || 3000
 # Specifies the `environment` that Puma will run in.
 #
 # environment ENV.fetch("RAILS_ENV") { "development" }
-environment ENV['RACK_ENV'] || 'development'
+# environment ENV['RACK_ENV'] || 'development'
+environment ENV['RACK_ENV'] || 'production'
 
 # Specifies the number of `workers` to boot in clustered mode.
 # Workers are forked webserver processes. If using threads and workers together
