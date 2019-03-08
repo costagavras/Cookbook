29751087383c6f60a9e099fb3e266534daacfc6d
costagavras <cgavras@yahoo.ca>
2019-03-07 22:13:59 -0500

    changed event listener for session button

--- app/assets/javascripts/session.js
+++ app/assets/javascripts/session.js
@@ -1,22 +1,23 @@
-document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener("readystatechange", function(){
+  if (document.readyState === "complete") {
+    if(document.querySelector(".log_in")){
+      var sign_up_form = document.querySelector(".sign_up_form")
+      var login = document.querySelector(".log_in");
+      var log_in_form = document.querySelector(".log_in_form");
+      var close = document.querySelector(".close_log_in")
 
-  if(document.querySelector(".log_in")){
-    var sign_up_form = document.querySelector(".sign_up_form")
-    var login = document.querySelector(".log_in");
-    var log_in_form = document.querySelector(".log_in_form");
-    var close = document.querySelector(".close_log_in")
+      login.addEventListener('click', function(event){
+        event.preventDefault();
+        alert("Hello");
+        log_in_form.style.display = "block";
+        sign_up_form.style.display = "none";
+      });
 
-    login.addEventListener('click', function(event){
-      event.preventDefault();
-      alert("Hello");
-      log_in_form.style.display = "block";
-      sign_up_form.style.display = "none";
-    });
+      close.addEventListener('click', function(){
+        event.preventDefault();
+        log_in_form.style.display = "none";
 
-    close.addEventListener('click', function(){
-      event.preventDefault();
-      log_in_form.style.display = "none";
-
-    });
+      });
+    }
   }
 });
