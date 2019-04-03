3ad462acf4bd762ea16cd5146d6b6f7bbdaa770f
costagavras <cgavras@yahoo.ca>
2019-04-03 17:48:39 -0400

    added romanian locale, corrected database route, changed language emoji for .png

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 # custom routes
-  scope ':locale', locale: /en|it|fr|ru/ do
+  scope ':locale', locale: /en|it|fr|ro|ru/ do
     root "welcome#index"
     get 'welcome/index'
     get '/', to: 'welcome#index'
     get '/welcome/about', :as => 'about'
-    get '/welcome/database', to: "recipes#database", :as => 'recipes_database'
+    get '/recipes/database', to: "recipes#database", :as => 'recipes_database'
     post '/recipes/search', to: "recipes#search", :as => 'recipe_search'
     post '/recipes/filter', to: "recipes#filter", :as => 'recipe_filter'
 
